d87ab0c821c098ca4e75dc2bd596cd48e93aa25e
nick-esqueda <nick_esqueda@outlook.com>
2022-04-13 10:45:33 -0500

    added search form and icon

--- react-app/src/components/SearchHeader/SearchHeader.js
+++ react-app/src/components/SearchHeader/SearchHeader.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 import './SearchHeader';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function SearchHeader() {
-  return (
-    <div>SearchHeader</div>
-  )
+    
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log('search submission');
+    }
+    
+    return (
+        <div className='search'>
+            <form className="header__search" onSubmit={onSubmit}>
+                <input type="text" placeholder="search"
+                    className=""
+                />
+                <FontAwesomeIcon icon={faSearch} style={{ color: 'var(--color-dark-gray)' }} />
+            </form>
+        </div>
+    )
 }
