25b8c2134045c624e1f34bd8255f932fc1a434f3
nick-esqueda <nick_esqueda@outlook.com>
2022-04-13 09:47:05 -0500

    WIP header right

--- react-app/src/components/Header/Header.js
+++ react-app/src/components/Header/Header.js
@@ -3,21 +3,41 @@ import React from 'react'
 import './Header.css';
 import logo from '../../static/yt_logo_rgb_dark.png'
 import hamburger from '../../static/icons/hamburger.png';
+import newVideo from '../../static/icons/new-video.png';
+import appsMenu from '../../static/icons/apps-menu.png';
+import bellIcon from '../../static/icons/bell-icon.png';
 
 export default function Header() {
-  return (
-    <header id='header' className='row-space-between'>
-      <div id='header__left' className='row-space-even'>
-        <div className='hamburger'>
-          <img src={hamburger} className="svg" />
-        </div>
-        
-        <div className="logo-wrapper">
-          <img src={logo} alt='logo' id='header__logo' className='' />
-        </div>
-      </div>
+	return (
+		<header id='header' className='row-space-between'>
+			<div id='header__left' className='row-space-even'>
+				<div className='svg-wrapper'>
+					<img src={hamburger} alt="menu-icon" className="svg" />
+				</div>
 
-      ausdfasdjfaskdjfasdf
-    </header>
-  )
+				<div className="logo-wrapper">
+					<img src={logo} alt='logo' id='header__logo' className='' />
+				</div>
+			</div>
+
+			<div id='header__middle' className='test'></div>
+
+
+			<div id='header__right' className='row-space-even test'>
+				<div className='svg-wrapper'>
+					<img src={newVideo} alt='bell-icon' className='svg' />
+				</div>
+				<div className='svg-wrapper'>
+					<img src={appsMenu} alt='apps-menu' className='svg' />
+
+				</div>
+				<div className='svg-wrapper'>
+					<img src={bellIcon} alt='bell-icon' className='svg' />
+				</div>
+				<div className='svg-wrapper'>
+
+				</div>
+			</div>
+		</header>
+	)
 }
