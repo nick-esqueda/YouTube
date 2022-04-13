8d2141504f49eed76b7b372f0c3bead8042be0a2
nick-esqueda <nick_esqueda@outlook.com>
2022-04-13 12:58:57 -0500

    set up and styled side navbar

--- react-app/src/components/Navbar/Navbar.js
+++ react-app/src/components/Navbar/Navbar.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
 import './Navbar.css';
+import home from '../../static/icons/home.png';
+import explore from '../../static/icons/explore.png';
+import subscriptions from '../../static/icons/subscriptions.png';
+import library from '../../static/icons/library.png';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <nav id="nav" className='test'>
-            ajdsfksa
+        <nav id="nav">
+            <Link to={`/`}>
+                <div className='svg-wrapper'>
+                    <img src={home} alt='bell-icon' className='svg' />
+                    Home
+                </div>
+            </Link>
+            <Link to={`/`}>
+                <div className='svg-wrapper'>
+                    <img src={explore} alt='apps-menu' className='svg' />
+                    Explore
+                </div>
+            </Link>
+            <Link to={`/`}>
+                <div className='svg-wrapper'>
+                    <img src={subscriptions} alt='bell-icon' className='svg' />
+                    Subscriptions
+                </div>
+            </Link>
+            <Link to={`/`}>
+                <div className='svg-wrapper'>
+                    <img src={library} alt='bell-icon' className='svg' />
+                    Library
+                </div>
+            </Link>
+
         </nav>
     )
 }
