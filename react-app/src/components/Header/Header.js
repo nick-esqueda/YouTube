ce4550fe31d915751ca0a7785f183f7f949f8431
nick-esqueda <nick_esqueda@outlook.com>
2022-04-13 08:53:27 -0500

    WIP header left

--- react-app/src/components/Header/Header.js
+++ react-app/src/components/Header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
 import './Header.css';
+import logo from '../../static/yt_logo_rgb_dark.png'
+import hamburger from '../../static/icons/hamburger.png';
 
 export default function Header() {
   return (
-    <header>Header</header>
+    <header id='header' className='row-space-between'>
+      <div id='header__left' className='row-space-even'>
+        <div className='hamburger'>
+          <img src={hamburger} className="svg" />
+        </div>
+        
+        <div className="logo-wrapper">
+          <img src={logo} alt='logo' id='header__logo' className='' />
+        </div>
+      </div>
+
+      ausdfasdjfaskdjfasdf
+    </header>
   )
 }
