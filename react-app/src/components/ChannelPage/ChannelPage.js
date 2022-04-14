e522665aaafa814457b8faee88d9f9c4e172a769
nick-esqueda <nick_esqueda@outlook.com>
2022-04-13 19:17:20 -0500

    adjusted Channel to_dict method

---
+++ react-app/src/components/ChannelPage/ChannelPage.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+import './ChannelPage.css';
+
+export default function ChannelPage() {
+	const [channel, setChannel] = useState({});
+	const { channelId } = useParams();k
+
+	useEffect(() => {
+		if (!channelId) {
+			return;
+		}
+		(async () => {
+			const response = await fetch(`/api/channels/${channelId}`);
+			const channel = await response.json();
+			setChannel(channel);
+		})();
+	}, [channelId]);
+
+	return (
+		<div id='channel-page' className='test'>
+			{!channel.bannerImageUrl ? null : (
+				<div id='channel-header'>
+					<img src={channel.bannerImageUrl} alt="banner" />
+				</div>
+			)}
+		</div>
+	)
+}
