d29ea5c8176024c7f943b4fea92ac828b379f261
nick-esqueda <nick_esqueda@outlook.com>
2022-04-13 19:14:38 -0500

    WIP channel page banner - SWITCH about to go to backend to change channel to-dict method to include banner image

--- react-app/src/components/ChannelPage/ChannelPage.js
+++ react-app/src/components/ChannelPage/ChannelPage.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 import './ChannelPage.css';
 
 export default function ChannelPage() {
-  return (
-    <div id='channel-page' className='test'>
-        <div id='channel-header'>
-            header
-        </div>
-    </div>
-  )
+	const [channel, setChannel] = useState({});
+	const { channelId } = useParams();
+
+	useEffect(() => {
+		if (!channelId) {
+			return;
+		}
+		(async () => {
+			const response = await fetch(`/api/channels/${channelId}`);
+			const channel = await response.json();
+			setChannel(channel);
+		})();
+	}, [channelId]);
+
+	return (
+		<div id='channel-page' className='test'>
+			{!channel.bannerImageUrl ? null : (
+				<div id='channel-header'>
+					<img src={channel.bannerImageUrl} alt="banner" />
+				</div>
+			)}
+		</div>
+	)
 }
