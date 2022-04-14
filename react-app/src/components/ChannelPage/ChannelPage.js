fb606f694d89013726722239e3f92288d57526be
nick-esqueda <nick_esqueda@outlook.com>
2022-04-13 20:38:46 -0500

    WIP channel header

--- react-app/src/components/ChannelPage/ChannelPage.js
+++ react-app/src/components/ChannelPage/ChannelPage.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
+import ProfileIcon from '../ProfileIcon/ProfileIcon';
+
 import './ChannelPage.css';
 
 export default function ChannelPage() {
 	const [channel, setChannel] = useState({});
+	const [activeTab, setActiveTab] = useState(1);
 	const { channelId } = useParams();
 
 	useEffect(() => {
@@ -26,9 +29,34 @@ export default function ChannelPage() {
 						<img src={channel.bannerImageUrl} alt="banner" />
 					</div>
 				)}
-				
-				<div id='channel-header__actions' className='test'>
-					
+
+				<div id='channel-header__actions' className='test2'>
+					<div className='channel-info row-space-between full-size test3'>
+						<div style={{ width: "80px", height: "80px" }}>
+							<ProfileIcon />
+						</div>
+					</div>
+					<div className='tabs row-space-between test4'>
+						<div id='channel__tabs-container' className='row-space-even'>
+							<div
+								className={activeTab === 1 ? 'tab active-tab' : 'tab'}
+								onClick={() => setActiveTab(1)}
+							>HOME</div>
+							<div
+								className={activeTab === 2 ? 'tab active-tab' : 'tab'}
+								onClick={() => setActiveTab(2)}
+							>VIDEOS</div>
+							<div
+								className={activeTab === 3 ? 'tab active-tab' : 'tab'}
+								onClick={() => setActiveTab(3)}
+							>CHANNELS</div>
+							<div
+								className={activeTab === 3 ? 'tab active-tab' : 'tab'}
+								onClick={() => setActiveTab(3)}
+							>ABOUT</div>
+						</div>
+
+					</div>
 				</div>
 			</div>
 		</div>
