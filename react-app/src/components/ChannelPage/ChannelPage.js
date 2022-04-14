ee25877c119eb75e92e6de5c25615794c53cd7b3
nick-esqueda <nick_esqueda@outlook.com>
2022-04-14 09:04:09 -0500

    adjusted styles for users without banners, adjusted channel page styles for different viewport sizes

--- react-app/src/components/ChannelPage/ChannelPage.js
+++ react-app/src/components/ChannelPage/ChannelPage.js
@@ -35,40 +35,42 @@ export default function ChannelPage() {
 					</div>
 				)}
 
-				<div id='channel-header__actions' className='col-left'>
-					<div className='channel-info row-space-between full-size'>
-						<div style={{ minWidth: "80px", height: "80px", marginRight: '20px' }}>
-							<ProfileIcon channel={channel} />
+				<div style={{ width: '100%', backgroundColor: 'var(--gray-darkest)' }}>
+					<div id='channel-header__actions' className='col-left'>
+						<div className='channel-info row-space-between full-size'>
+							<div style={{ minWidth: "80px", height: "80px", marginRight: '20px' }}>
+								<ProfileIcon channel={channel} />
+							</div>
+
+							<div className='col-left full-size'>
+								<h2>{channel.channelName}</h2>
+								<span className='subcount'>[# of subscribers]</span>
+							</div>
+
+							<button className='btn btn--red disabled'>
+								SUBSCRIBE
+							</button>
 						</div>
 
-						<div className='col-left full-size'>
-							<h2>{channel.channelName}</h2>
-							<span className='subcount'>[# of subscribers]</span>
-						</div>
-
-						<button className='btn btn--red disabled'>
-							SUBSCRIBE
-						</button>
-					</div>
-
-					<div className='tabs row-space-between'>
-						<div id='channel__tabs-container' className='row-space-even'>
-							<NavLink to={`${url}/home`}
-								className={activeTab === 1 ? 'tab active-tab' : 'tab'}
-								onClick={() => setActiveTab(1)}
-							>HOME</NavLink>
-							<NavLink to={`${url}/videos`}
-								className={activeTab === 2 ? 'tab active-tab' : 'tab'}
-								onClick={() => setActiveTab(2)}
-							>VIDEOS</NavLink>
-							<NavLink to={`${url}/channels`}
-								className={activeTab === 3 ? 'tab active-tab' : 'tab'}
-								onClick={() => setActiveTab(3)}
-							>CHANNELS</NavLink>
-							<NavLink to={`${url}/about`}
-								className={activeTab === 4 ? 'tab active-tab' : 'tab'}
-								onClick={() => setActiveTab(4)}
-							>ABOUT</NavLink>
+						<div className='tabs row-space-between'>
+							<div id='channel__tabs-container' className='row-space-even'>
+								<NavLink to={`${url}/home`}
+									className={activeTab === 1 ? 'tab active-tab' : 'tab'}
+									onClick={() => setActiveTab(1)}
+								>HOME</NavLink>
+								<NavLink to={`${url}/videos`}
+									className={activeTab === 2 ? 'tab active-tab' : 'tab'}
+									onClick={() => setActiveTab(2)}
+								>VIDEOS</NavLink>
+								<NavLink to={`${url}/channels`}
+									className={activeTab === 3 ? 'tab active-tab' : 'tab'}
+									onClick={() => setActiveTab(3)}
+								>CHANNELS</NavLink>
+								<NavLink to={`${url}/about`}
+									className={activeTab === 4 ? 'tab active-tab' : 'tab'}
+									onClick={() => setActiveTab(4)}
+								>ABOUT</NavLink>
+							</div>
 						</div>
 					</div>
 				</div>
