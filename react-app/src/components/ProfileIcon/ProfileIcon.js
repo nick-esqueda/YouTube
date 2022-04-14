a2bcd39d8bbfb1e8a0b2e75682f85864fb3161a5
nick-esqueda <nick_esqueda@outlook.com>
2022-04-14 09:14:27 -0500

    set profile icon in header to direct to user's channel

--- react-app/src/components/ProfileIcon/ProfileIcon.js
+++ react-app/src/components/ProfileIcon/ProfileIcon.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 
 import './ProfileIcon.css';
 
 export default function ProfileIcon({ channel }) {
+    const history = useHistory();
     return (
         <div
             className='profile-icon-border'
-            // onClick={(e) => {
-            //     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-            //     // TODO: redirect to channel here
-            // }}
+            onClick={(e) => {
+                window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+                history.push(`/channels/${channel.id}`);
+            }}
         >
             <img
                 className='profile-icon'
