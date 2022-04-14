39bbe0943bf604dcd9ef87ae71e2887b4e722a72
nick-esqueda <nick_esqueda@outlook.com>
2022-04-14 08:02:03 -0500

    layout/styles for channel page about tab

--- react-app/src/components/ChannelPage/AboutTab.js
+++ react-app/src/components/ChannelPage/AboutTab.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-import './AboutTab';
+import './AboutTab.css';
 
 export default function AboutTab({ channel }) {
-  return (
-    <div>{channel.about}</div>
-  )
+    return (
+        <div id='about-tab' className='row-top'>
+            <div id='about-tab-left' className='col-left'>
+                <h4>Description</h4>
+                <p style={{ fontSize: '14px' }}>{channel.about}</p>
+            </div>
+            <div id='about-tab-right' className='col-left'>
+                <span style={{ paddingTop: '0' }}>Stats</span>
+                <span>Joined {channel.createdAt}</span>
+                <span>[view count]</span>
+            </div>
+        </div>
+    )
 }
