e45ead545e39aee9d52a21e5619b9f73babff79b
nick-esqueda <nick_esqueda@outlook.com>
2022-04-15 08:53:06 -0500

    set up action creators for videos (NO TEST)

--- react-app/src/store/videos.js
+++ react-app/src/store/videos.js
@@ -6,6 +6,34 @@ const REMOVE_VIDEO = 'videos/REMOVE_VIDEO';
 
 
 // ACTION CREATORS ****************************************
+// POSTS
+const addVideo = (video) => {
+    return {
+        type: ADD_VIDEO,
+        post
+    }
+}
+
+// const loadVideos = (videos) => {
+//     return {
+//         type: LOAD_VIDEOS,
+//         videos
+//     }
+// }
+
+const loadAdditionalVideos = (videos) => {
+    return {
+        type: LOAD_ADDITIONAL_VIDEOS,
+        videos
+    }
+}
+
+const removeVideo = (videoId) => {
+    return {
+        type: REMOVE_VIDEO,
+        postId
+    }
+}
 
 
 
