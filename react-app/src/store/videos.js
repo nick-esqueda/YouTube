27d742a3d85c5da47e2a26be4f6dc594c278c3df
nick-esqueda <nick_esqueda@outlook.com>
2022-04-15 08:36:26 -0500

    created and connected videos reducer

---
+++ react-app/src/store/videos.js
@@ -0,0 +1,23 @@
+// ACTION VARIABLES ***************************************
+
+
+
+// ACTION CREATORS ****************************************
+
+
+
+// THUNK ACTION CREATORS **********************************
+
+
+
+// REDUCER ************************************************
+const videosReducer = (state = {}, action) => {
+    
+    switch (action.type) {
+        
+        default:
+            return state;
+    }
+}
+
+export default videosReducer;
