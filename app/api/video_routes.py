53a8ff448b53c81ec5afab556c6562d5ec90084f
nick-esqueda <nick_esqueda@outlook.com>
2022-04-15 07:50:41 -0500

    created video GET routes (NO TEST)

--- app/api/video_routes.py
+++ app/api/video_routes.py
@@ -1,9 +1,34 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session
+from sqlalchemy import desc
 from app.models import Video, Channel
 
 video_routes = Blueprint('videos', __name__)
 
 
-@video_routes.route('/<int:videoId>')
-def get_video(id):
-    pass
+@video_routes.route('/<int:videoId>/')
+def get_video(videoId):
+    """
+    GET /api/videos/:videoId\n
+    Get a single video and its associated data.
+    NOTE: 
+    """
+    video = Video.query.get(videoId).to_dict()
+    return jsonify(video)
+
+
+@video_routes.route('pages/<int:pageNum>/')
+def get_videos(pageNum):
+    """
+    GET /api/videos/ \n
+    Get all videos, for the home page. \n
+    NOTE: for now, just respond with all videos, paginated. \n
+    goal is to send suggested videos based on session user id
+    """
+    # id = int(session['_user_id'])
+    # sessionUser = Channel.query.get(id)
+
+    videos = Video.query.order_by(desc(Video.createdAt)).paginate(page=pageNum, per_page=10, error_out=False)
+    videos = [video.to_dict() for video in videos.items]
+
+    return jsonify(videos)
+    
