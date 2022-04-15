10f7d2160e258b71bc5aa6cf8b243343fefb8f98
nick-esqueda <nick_esqueda@outlook.com>
2022-04-15 11:55:19 -0500

    set up basic home page logic, tested a console log of the fetchHomeVideos thunk and received the array from the backend

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { fetchHomeVideos } from '../../store/videos';
 
 import './HomePage.css';
 
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+    const dispatch = useDispatch();
+
+    // trading this out so that we don't have to Object.values the useSelector
+    // const videos = useSelector(state => state.videos);
+    const [videos, setVideos] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [pageNum, setPageNum] = useState(2);
+    
+    useEffect(() => {
+        (async () => {
+            const videoArr = await dispatch(fetchHomeVideos(1))
+            console.log(videoArr, '\n\n\n');
+            setVideos(videoArr);
+            setIsLoaded(true);
+        })()
+    }, [dispatch]);
+    
+    useEffect(() => {
+        // try setting a new isLoaded to false then true to display loading spinner when getting next page?
+             
+    }, [pageNum])
+    
+    return isLoaded ? null : (
+        <div id='home-page' className='test1 col-space-even'>
+            <div className='video-row-grid test1'>
+                {videos.map(video => (
+                    <div key={video.id}>
+                        {video.title}
+                    </div>
+                        
+                ))}
+            </div>
+        </div>
+    )
 }
