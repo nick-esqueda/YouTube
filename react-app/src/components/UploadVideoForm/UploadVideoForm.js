2c3df01676ff8ac0cda73d1233232f1cd38bfe65
nick-esqueda <nick_esqueda@outlook.com>
2022-04-15 16:08:39 -0500

    tested iframe with non-youtube src

--- react-app/src/components/UploadVideoForm/UploadVideoForm.js
+++ react-app/src/components/UploadVideoForm/UploadVideoForm.js
@@ -3,7 +3,18 @@ import React from 'react'
 import './UploadVideoForm.css';
 
 export default function UploadVideoForm() {
-  return (
-    <div>UploadVideoForm</div>
-  )
+    return (
+        <div>
+            adfasdfasdf
+            <div>
+                <iframe width="560" height="315"
+                    src={`https://youtube-bucket-nick-esqueda.s3.amazonaws.com/lizard-vibing.mp4`}
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+            </div>
+        </div>
+    )
 }
