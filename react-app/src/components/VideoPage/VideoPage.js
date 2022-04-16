12a9a493493dfcb2c5b3a3dd2cca4cc117061339
nick-esqueda <nick_esqueda@outlook.com>
2022-04-15 22:54:17 -0500

    adjusted video description styles

--- react-app/src/components/VideoPage/VideoPage.js
+++ react-app/src/components/VideoPage/VideoPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
 import { fetchVideo } from '../../store/videos';
+import ProfileIcon from '../ProfileIcon/ProfileIcon';
 
 import './VideoPage.css';
 
@@ -34,25 +35,44 @@ export default function VideoPage() {
                     ></iframe>
                 </div>
 
-                <div className='video-info col-left test3'>
+                <div className='video-info col-left'>
                     <h4>{video.title}</h4>
                     <span className='subcount'>[# of views] * {video.createdAt}</span>
                 </div>
 
-                <div className='video-description col-left test4'>
-                    <p className={showMore ? "show-more" : "show-less"}>{video.description}</p>
-                    {!showMore
-                        ? <span
-                            className='subcount pointer'
-                            onClick={e => setShowMore(true)}
-                        >SHOW MORE</span>
-                        : <span
-                            className='subcount pointer'
-                            onClick={e => setShowMore(false)}
-                        >SHOW LESS</span>
-                    }
+                {/* <div className='test1 '>
+
+                </div> */}
+
+
+
+                <div className='video-description row-left row-top'>
+                    <div style={{ minWidth: "52px", height: "52px", marginRight: '20px' }}>
+                        <ProfileIcon channel={video.channel} />
+                    </div>
+
+
+                    <div className='col-left'>
+                        <div className='col-space-even full-size channel-and-subs'>
+                            <h4>{video.channel.channelName}</h4>
+                            <span className='subcount'>[# of subscribers]</span>
+                        </div>
+                        
+                        <p className={showMore ? "show-more" : "show-less"}>{video.description}</p>
+                        {!showMore
+                            ? <span
+                                className='subcount pointer show'
+                                onClick={e => setShowMore(true)}
+                            >SHOW MORE</span>
+                            : <span
+                                className='subcount pointer show'
+                                onClick={e => setShowMore(false)}
+                            >SHOW LESS</span>
+                        }
+                    </div>
                 </div>
             </div>
+
 
             <div className='right test1'>
                 fj adfjaadjajkfjkfd adf
