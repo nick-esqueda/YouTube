1c9660f13ab4b016928d2fe1189f7a053ef0c6a9
nick-esqueda <nick_esqueda@outlook.com>
2022-04-20 17:21:16 -0500

    styled the login page and set styles to indicate validation errors

--- react-app/src/components/LoginPage/LoginForm.js
+++ react-app/src/components/LoginPage/LoginForm.js
@@ -6,74 +6,89 @@ import { login } from '../../store/session';
 import './LoginPage.css';
 
 const LoginForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+	const [errors, setErrors] = useState([]);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const user = useSelector(state => state.session.user);
+	const dispatch = useDispatch();
 
-  const onLogin = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login(email, password));
-    if (data) {
-      setErrors(data);
-    }
-  };
-  
-  const loginDemoUser = async () => {
-    await dispatch(login('demo@aa.io', 'password'));
-  }
+	const onLogin = async (e) => {
+		e.preventDefault();
+		const data = await dispatch(login(email, password));
+		if (data) {
+			setErrors(data);
+		}
+	};
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
+	const loginDemoUser = async () => {
+		await dispatch(login('demo@aa.io', 'password'));
+	}
 
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
+	const updateEmail = (e) => {
+		setEmail(e.target.value);
+	};
 
-  if (user) {
-    return <Redirect to='/' />;
-  }
+	const updatePassword = (e) => {
+		setPassword(e.target.value);
+	};
 
-  return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit' className='btn btn--blue-outline'>Login</button>
-      </div>
+	if (user) {
+		return <Redirect to='/' />;
+	}
 
-      <div>
-        <Link to='/signup' style={{ color: 'var(--blue)' }}>Don't have an account yet?</Link>
-      </div>
+	return (
+		<div id="login-page">
+			<form onSubmit={onLogin} id='login-form'>
+				<div className='col-right'>
+					{errors.map((error, ind) => (
+						<span key={ind} className="subcount">{error.toUpperCase()}</span>
+					))}
+				</div>
 
-      <div>
-        <button type='button' onClick={loginDemoUser} className='btn btn--blue-outline'>demo</button>
-      </div>
-    </form>
-  );
+				<h2 style={{ fontSize: '20px', fontWeight: '500' }}>Email</h2>
+				<input
+					name='email'
+					type='text'
+					placeholder='Email'
+					value={email}
+					onChange={updateEmail}
+					style={
+						errors.find(
+							err => err.toUpperCase() === 'EMAIL : THIS FIELD IS REQUIRED.'
+								|| err.toUpperCase() === 'EMAIL : EMAIL PROVIDED NOT FOUND.')
+							? { borderColor: 'var(--red)' }
+							: {}
+					}
+				/>
+
+				<h2 style={{ fontSize: '20px', fontWeight: '500' }}>Password</h2>
+				<input
+					name='password'
+					type='password'
+					placeholder='Password'
+					value={password}
+					onChange={updatePassword}
+					style={
+						errors.find(
+							err => err.toUpperCase() === 'PASSWORD : THIS FIELD IS REQUIRED.'
+								|| err.toUpperCase() === 'PASSWORD : PASSWORD WAS INCORRECT.'
+								|| err.toUpperCase() === 'PASSWORD : NO SUCH USER EXISTS.')
+							? { borderColor: 'var(--red)' }
+							: {}
+					}
+				/>
+
+				<div className='row-space-between row-bottom' style={{ gap: '16px' }}>
+					<Link to='/signup' style={{ color: 'var(--blue)', fontSize: '14px' }}>Don't have an account yet?</Link>
+
+					<div className='row-right'>
+						<button type='button' onClick={loginDemoUser} className='btn'>demo</button>
+						<button type='submit' className='btn btn--blue-outline'>Login</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default LoginForm;
