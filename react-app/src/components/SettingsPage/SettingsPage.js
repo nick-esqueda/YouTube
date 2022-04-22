2501ba94807ac5017e0ee7c36d7ab743c0c5875b
nick-esqueda <nick_esqueda@outlook.com>
2022-04-21 22:52:48 -0500

    added frontend logic to test backend PATCH route

--- react-app/src/components/SettingsPage/SettingsPage.js
+++ react-app/src/components/SettingsPage/SettingsPage.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 import './SettingsPage.css';
 
 export default function SettingsPage() {
+	const dispatch = useDispatch();
+	const history = useHistory();
+	
+	const [profileImageUrl, setProfileImageUrl] = useState('');
+	const [bannerImageUrl, setBannerImageUrl] = useState('');
+	const [about, setAbout] = useState('');
+	
+	const onSubmit = e => {
+		e.preventDefault();
+		
+		// other stuff
+	}
+	
 	return (
 		<div id='settings-page' className=' test4'>
 			<div className='test1 image-upload-container left1'>
@@ -14,6 +29,14 @@ export default function SettingsPage() {
 			
 			<div className='test3 right'>
 				[title and about section edit here]
+				<form onSubmit={onSubmit}>
+					<input type='text' name='about'
+						id='about-input'
+						value={about}
+						onChange={e => setAbout(e.target.value)}
+					/>
+						
+				</form>
 			</div>
 		</div>
 	)
