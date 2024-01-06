873f2067b42ed37b5d053f9ba4a92713b65ff14d
nick-esqueda <nick_esqueda@outlook.com>
2024-01-06 16:24:27 -0600

    search: created search API. formatted application.py

--- backend/application.py
+++ backend/application.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -11,6 +10,7 @@ from api.channel_routes import channel_routes
 from api.video_routes import video_routes
 from api.comment_routes import comment_routes
 from api.s3_routes import s3_routes
+from api.search_routes import search_routes
 
 from seeds import seed_commands
 
@@ -20,7 +20,7 @@ application = Flask(__name__)
 
 # Setup login manager
 login = LoginManager(application)
-login.login_view = 'auth.unauthorized'
+login.login_view = "auth.unauthorized"
 
 
 @login.user_loader
@@ -32,11 +32,12 @@ def load_user(id):
 application.cli.add_command(seed_commands)
 
 application.config.from_object(Config)
-application.register_blueprint(auth_routes, url_prefix='/api/auth')
-application.register_blueprint(channel_routes, url_prefix='/api/channels')
-application.register_blueprint(video_routes, url_prefix='/api/videos')
-application.register_blueprint(comment_routes, url_prefix='/api/videos')
-application.register_blueprint(s3_routes, url_prefix='/api/s3')
+application.register_blueprint(auth_routes, url_prefix="/api/auth")
+application.register_blueprint(channel_routes, url_prefix="/api/channels")
+application.register_blueprint(video_routes, url_prefix="/api/videos")
+application.register_blueprint(comment_routes, url_prefix="/api/videos")
+application.register_blueprint(s3_routes, url_prefix="/api/s3")
+application.register_blueprint(search_routes, url_prefix="/api/search")
 db.init_app(application)
 Migrate(application, db)
 
@@ -47,19 +48,15 @@ CORS(application, supports_credentials=True)
 @application.after_request
 def inject_csrf_token(response):
     response.set_cookie(
-        'csrf_token',
-        generate_csrf(),
-        secure=True,
-        samesite='None',
-        httponly=True)
+        "csrf_token", generate_csrf(), secure=True, samesite="None", httponly=True
+    )
     return response
 
 
-@application.route('/health')
+@application.route("/health")
 def health_check():
-    return {'success': True}, 200, {'ContentType':'application/json'}
+    return {"success": True}, 200, {"ContentType": "application/json"}
 
 
 if __name__ == "__main__":
     application.run(debug=True)
-
