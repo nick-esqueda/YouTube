3a51acfd622d71004c39a5884067806b92c876c7
nick-esqueda <nick_esqueda@outlook.com>
2024-05-16 23:40:25 -0500

    aws migration: migrated to new AWS profile and modified infra scripts

--- scripts/stop-infra-prod.sh
+++ scripts/stop-infra-prod.sh
@@ -1,29 +1,37 @@
+# RUN THIS SCRIPT FROM THE PROJECT ROOT DIRECTORY.
+
+if [ -f scripts/.env ]; then
+  source scripts/.env
+else 
+  echo "ERROR: .env file must exist in the ./scripts/ directory. Aborting script."
+  exit 1
+fi
+
 cd backend/
 pwd
 
-echo "This script will shut down the PROD EB environment (DB, EC2 instances, ELB). Are you sure you want to proceed? (yes/no)"
-read confirmation
-
-if [ "$confirmation" == "yes" ]; then
-    echo "Proceeding with the rest of the script..."
-else
-    echo "Script aborted."
-    exit 1
-fi
-
 echo 'triggering RDS DB shutdown...'
-aws rds stop-db-instance --no-cli-pager --db-instance-identifier awseb-e-8cucwt8iij-stack-awsebrdsdatabase-jfnalsxu5jlf
 
-echo 'killing load balancer...'
+aws rds stop-db-instance \
+--no-cli-pager \
+--db-instance-identifier $RDS_DB_INSTANCE_ID_PROD \
+--profile $AWS_PROFILE 
+
+echo 'removing load balancer from EB environment...'
+
 cd .elasticbeanstalk
-eb config youtube-prod --update file://update-to-single-instance-env.yml
+eb config youtube-prod \
+--update file://update-to-single-instance-env.yml \
+--profile $AWS_PROFILE 
 cd ..
 
-echo 'scaling down EC2 instances to 0...'
+echo 'scaling EC2 instances down to 0...'
+
 aws autoscaling update-auto-scaling-group \
---auto-scaling-group-name awseb-e-8cucwt8iij-stack-AWSEBAutoScalingGroup-mctoWswbQr2G \
+--auto-scaling-group-name $AUTO_SCALING_GROUP_NAME_PROD \
 --min-size 0 \
 --max-size 1 \
---desired-capacity 0
+--desired-capacity 0 \
+--profile $AWS_PROFILE 
 
 echo 'script completed. some AWS processes may still be running.'
